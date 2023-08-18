c903beb9324c8e6586f6707b6d71703b5c0680fe
acac1581 <acac1581@gmail.com>
2023-08-18 16:48:23 +0700

    up

---
+++ docs/cocos-js/wait-for-ammo-instantiation.a54cf.js
@@ -0,0 +1,1 @@
+System.register(["./instantiated-f2dc9552.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.hR)}],execute:function(){}}}));
--- docs/cocos-js/wait-for-ammo-instantiation.f0d48.js
+++
@@ -1,1 +0,0 @@
-System.register(["./instantiated-46ec2385.js"],(function(t){"use strict";return{setters:[function(e){t("default",e.hQ)}],execute:function(){}}}));
