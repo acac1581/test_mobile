cc9e0405cb77b90f4d948d15d8d4e5f763c48ffa
acac1581 <acac1581@gmail.com>
2024-10-03 09:58:07 +0700

    Up1

--- docs/application.3736b.js
+++ docs/application.3736b.js
@@ -14,7 +14,7 @@ System.register([], function (_export, _context) {
       _export("Application", Application = /*#__PURE__*/function () {
         function Application() {
           _classCallCheck(this, Application);
-          this.settingsPath = 'src/settings.f911b.json';
+          this.settingsPath = 'src/settings.aa089.json';
           this.showFPS = false;
         }
         _createClass(Application, [{
--- docs/index.de8f2.js
+++ docs/index.de8f2.js
@@ -1,4 +1,4 @@
-System.register(["./application.bbaeb.js"], function (_export, _context) {
+System.register(["./application.3736b.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
