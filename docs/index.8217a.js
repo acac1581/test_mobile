5d6e3f34e897109668754f0fad0cedb33f7e85b9
acac1581 <acac1581@gmail.com>
2024-11-24 20:33:58 +0700

    build

--- docs/index.8217a.js
+++ docs/index.8217a.js
@@ -1,4 +1,4 @@
-System.register(["./application.88a43.js"], function (_export, _context) {
+System.register(["./application.fac08.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
