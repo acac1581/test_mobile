b3fdec1346f7c1e3dd2054a34730ac0f62543c41
acac1581 <acac1581@gmail.com>
2024-11-24 20:53:50 +0700

    build

--- docs/application.1d05e.js
+++ docs/application.1d05e.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.9f80f.json';
+          this.settingsPath = 'src/settings.07771.json';
           this.showFPS = true;
         }
         init(engine) {
--- docs/index.d0cba.js
+++ docs/index.d0cba.js
@@ -1,4 +1,4 @@
-System.register(["./application.fac08.js"], function (_export, _context) {
+System.register(["./application.1d05e.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
