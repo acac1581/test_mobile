c3580384da69b7115494e534bec40c2da34f32c3
acac1581 <acac1581@gmail.com>
2024-12-09 03:46:17 +0700

    build

--- docs/application.0fdcd.js
+++ docs/application.0fdcd.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.11b88.json';
+          this.settingsPath = 'src/settings.dd60d.json';
           this.showFPS = true;
         }
         init(engine) {
