97bea7c9b5ba431182aa81f879e55cbc66f1f2c0
acac1581 <acac1581@gmail.com>
2024-12-30 20:12:27 +0700

    b3

--- docs/index.261a6.js
+++ docs/index.261a6.js
@@ -1,4 +1,4 @@
-System.register(["./application.33d30.js"], function (_export, _context) {
+System.register(["./application.dd6ad.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
