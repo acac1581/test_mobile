ef2b23648b614f9d07c894e67e647911d08df8b5
acac1581 <acac1581@gmail.com>
2024-12-30 18:45:24 +0700

    build 3

--- docs/application.ae2de.js
+++ docs/application.ae2de.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.3f951.json';
+          this.settingsPath = 'src/settings.5449d.json';
           this.showFPS = true;
         }
         init(engine) {
--- docs/index.c08c4.js
+++ docs/index.c08c4.js
@@ -1,4 +1,4 @@
-System.register(["./application.5512a.js"], function (_export, _context) {
+System.register(["./application.ae2de.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
