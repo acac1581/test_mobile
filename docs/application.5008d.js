d7002f260d44a7836f9423d914802e4823ba4296
acac1581 <acac1581@gmail.com>
2024-12-30 16:45:25 +0700

    build 1

--- docs/application.5008d.js
+++ docs/application.5008d.js
@@ -8,7 +8,7 @@ System.register([], function (_export, _context) {
     execute: function () {
       _export("Application", Application = class Application {
         constructor() {
-          this.settingsPath = 'src/settings.dd60d.json';
+          this.settingsPath = 'src/settings.ad5d9.json';
           this.showFPS = true;
         }
         init(engine) {
