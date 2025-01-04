fc6443d092e72f00bf53babe9493a1980284639f
acac1581 <acac1581@gmail.com>
2025-01-04 14:51:55 +0700

    build

--- docs/index.1ca44.js
+++ docs/index.1ca44.js
@@ -1,4 +1,4 @@
-System.register(["./application.d4552.js"], function (_export, _context) {
+System.register(["./application.2ecda.js"], function (_export, _context) {
   "use strict";
 
   var Application, canvas, $p, bcr, application;
